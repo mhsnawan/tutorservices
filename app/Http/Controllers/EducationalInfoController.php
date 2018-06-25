0f71eda33a4d9f6690dc314c3203d19842442954
mhsnawan <mohsin.awan0@gmail.com>
2018-06-26 02:52:23 +0500

    Added educationalInfoController m c

---
+++ app/Http/Controllers/EducationalInfoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\EducationalInfo;
+use Illuminate\Http\Request;
+
+class EducationalInfoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\EducationalInfo  $educationalInfo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EducationalInfo $educationalInfo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\EducationalInfo  $educationalInfo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EducationalInfo $educationalInfo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\EducationalInfo  $educationalInfo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EducationalInfo $educationalInfo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\EducationalInfo  $educationalInfo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EducationalInfo $educationalInfo)
+    {
+        //
+    }
+}
