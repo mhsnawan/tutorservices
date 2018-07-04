48aa9fb4398c6f4936c43a0dcc1082264255e779
mhsnawan <mohsin.awan0@gmail.com>
2018-07-05 00:46:53 +0500

    Added Course -mcr

---
+++ app/Course.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    protected $fillable = [
+        'course_id','course_name'
+    ];
+    
+    public $timestamps = false;
+}
