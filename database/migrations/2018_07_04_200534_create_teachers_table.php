c0ea473ad886f70f8b5fc6dad351892381a914a4
mhsnawan <mohsin.awan0@gmail.com>
2018-07-05 01:08:34 +0500

    Added student and teacher -mcr with updated migrations

---
+++ database/migrations/2018_07_04_200534_create_teachers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->increments('teacher_id');
+            $table->Integer('user_id')->unsigned();
+            $table->foreign('user_id')->reference('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+}
